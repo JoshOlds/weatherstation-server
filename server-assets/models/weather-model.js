8187b631c670870c209a6958d3b2af5e665c1c6e
Josh Olds <joshua.olds@outlook.com>
2016-11-06 12:17:02 -0700

    reformatted weather structure

--- server-assets/models/weather-model.js
+++ server-assets/models/weather-model.js
@@ -8,35 +8,34 @@ let Weather = DS.defineResource({
   endpoint: 'weather'
 })
 
-/*
-Example weather object
-exampleWeather = {
-    dateStamp: date
-    reading: {
-        tempF: 50.02,
-        tempC: 10.10,
-        rh: 49.01
-    }
-}
-*/
-
-
 function create(weather, cb) {
     // Use the Resource Model to create a new weather entry
     if(!weather.dateStamp || !weather.reading){
         return new Error("Invalid weather format")
     }
     let dateStamp = weather.dateStamp;
+    let dateId = new Date(dateStamp).toString().substring(0, 15);
     let tempF = weather.reading.tempF.toFixed(2);
-    let tempC = weather.reading.tempF.toFixed(2);
+    let tempC = weather.reading.tempC.toFixed(2);
     let rh = weather.reading.rh.toFixed(2);
+    let reading = {timeStamp: dateStamp, tempF: tempF, tempC: tempC, rh: rh}
 
-    let cleanWeather = { id: uuid.v4(), dateStamp: dateStamp, reading:{tempF: tempF, tempC: tempC, rh: rh} }
-    Weather.create(cleanWeather).then(cb).catch(cb)
+    getById(dateId, "",  data => {
+      if(data.message){ //First post of day
+        let post = {id: dateId, readings: [reading]}
+        Weather.create(post).then(cb).catch(cb);
+      }else{
+        let readings = data.readings;
+        readings.push(reading);
+        let post = {id: dateId, readings: readings};
+        Weather.create(post).then(cb).catch(cb);
+      }
+    })
+    
 }
 
 function getAll(query, cb) {
-  //Use the Resource Model to get all Galaxies
+  //Use the Resource Model to get all weather
   Weather.findAll({}).then(cb).catch(cb)
 }
 
